8e1f23cb5bb1eedb7b36dc445407be2566de5214
turanct <spinnewebber_toon@hotmail.com>
2017-12-10 14:44:09 +0100

    ✨ Creating some Knot Hashes in Haskell... easier said than done

--- src/Day10.hs
+++ src/Day10.hs
@@ -1,5 +1,9 @@
 module Day10 where
 
+import Data.Char
+import Data.Bits
+import Numeric
+
 type Circle = [Int]
 type Position = Int
 type Length = Int
@@ -22,3 +26,27 @@ knot s l = State { circle = newcircle, position = newposition, skipSize = newski
 
 checksum :: State -> Int
 checksum s = foldl (*) 1 $ take 2 $ circle s
+
+lengthsFromString :: String -> [Int]
+lengthsFromString s = concat $ replicate 64 ascii
+  where ascii = stringToAsciiList s ++ [17, 31, 73, 47, 23]
+        stringToAsciiList = map (ord)
+
+sparseHashToDenseHash :: [Int] -> [Int]
+sparseHashToDenseHash xs = map (foldl xor 0) $ groupBy16 xs
+  where groupBy16 [] = []
+        groupBy16 xs
+          | length xs < 16 = xs : []
+          | otherwise = take 16 xs : (groupBy16 $ drop 16 xs)
+
+asHex :: [Int] -> String
+asHex is = concat $ map (pad . intToHex) is
+  where intToHex i = showHex i ""
+        pad [a] = ['0', a]
+        pad s = s
+
+knotHash :: String -> String
+knotHash i = asHex $ sparseHashToDenseHash $ circle $ foldl knot state $ lengthsFromString i
+  where state = State { circle = [0..255]
+                      , position = 0
+                      , skipSize = 0 }
--- test/Day10Spec.hs
+++ test/Day10Spec.hs
@@ -37,3 +37,20 @@ spec = do
       let lengths = [225,171,131,2,35,5,0,13,1,246,54,97,255,98,254,110]
       let lotsOfKnots = foldl knot state lengths
       checksum lotsOfKnots `shouldBe` 23874
+
+  describe "knotHash" $ do
+    it "converts a string to a hash 1" $ do
+      knotHash "" `shouldBe` "a2582a3a0e66e6e86e3812dcb672a272"
+
+    it "converts a string to a hash 2" $ do
+      knotHash "AoC 2017" `shouldBe` "33efeb34ea91902bb2f59c9920caa6cd"
+
+    it "converts a string to a hash 3" $ do
+      knotHash "1,2,3" `shouldBe` "3efbe78a8d82f29979031a4aa0b16a9d"
+
+    it "converts a string to a hash 4" $ do
+      knotHash "1,2,4" `shouldBe` "63960835bcdc130f0b66d7ff4f6a5a8e"
+
+    it "solves the puzzle" $ do
+      let input = "225,171,131,2,35,5,0,13,1,246,54,97,255,98,254,110"
+      knotHash input `shouldBe` "e1a65bfb5a5ce396025fab5528c25a87"
