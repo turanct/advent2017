4a64524eb14753a5751184e92938902459adda0f
turanct <spinnewebber_toon@hotmail.com>
2017-12-12 19:10:27 +0100

    🍰 Solved Day 12!

--- src/Day12.hs
+++ src/Day12.hs
@@ -1,5 +1,7 @@
 module Day12 where
 
+import Data.List
+
 type Process = String
 type Connection = (Process, Process)
 
@@ -13,3 +15,24 @@ splitByComma "" = []
 splitByComma s = firstpart : splitByComma rest
   where firstpart = takeWhile (/= ',') $ dropWhile (== ' ') s
         rest = drop 1 $ dropWhile (/= ',') s
+
+directConnections :: [Connection] -> Process -> [Process]
+directConnections cs p = map snd $ filter ((== p) . fst) cs
+
+findGroup :: [Connection] -> Process -> [Process]
+findGroup cs p = findGroup' cs [] p
+  where findGroup' cs ps p
+          | p `elem` ps = ps
+          | otherwise = nub $ concat $ map (findGroup' cs (p:ps)) (directConnections cs p)
+
+part1 :: [Connection] -> Process -> Int
+part1 cs p = length $ findGroup cs p
+
+findGroups :: [Connection] -> [[Process]]
+findGroups cs = foldl addGroupIfNotPresent [] cs
+  where addGroupIfNotPresent groups connection
+          | fst connection `elem` concat groups = groups
+          | otherwise = findGroup cs (fst connection) : groups
+
+part2 :: [Connection] -> Int
+part2 = length . findGroups
--- test/Day12Spec.hs
+++ test/Day12Spec.hs
@@ -11,3 +11,50 @@ spec = do
 
     it "reads connections from string 2" $ do
       readConnections "4 <-> 2, 3, 6" `shouldBe` [("4", "2"), ("4", "3"), ("4", "6")]
+
+  describe "direct connections" $ do
+    let cs = [ ("0", "2"), ("1", "1"), ("2", "0"), ("2", "3"), ("2", "4")
+             , ("3", "2"), ("3", "4"), ("4", "2"), ("4", "3"), ("4", "6")
+             , ("5", "6"), ("6", "4"), ("6", "5") ]
+
+    it "finds direct connections 1" $ do
+      directConnections cs "9" `shouldBe` []
+
+    it "finds direct connections 2" $ do
+      directConnections cs "1" `shouldBe` ["1"]
+
+    it "finds direct connections 3" $ do
+      directConnections cs "2" `shouldBe` ["0", "3", "4"]
+
+  describe "find group" $ do
+    let cs = [ ("0", "2"), ("1", "1"), ("2", "0"), ("2", "3"), ("2", "4")
+             , ("3", "2"), ("3", "4"), ("4", "2"), ("4", "3"), ("4", "6")
+             , ("5", "6"), ("6", "4"), ("6", "5") ]
+
+    it "find the group with one member" $ do
+      findGroup cs "1" `shouldBe` ["1"]
+
+    it "finds the complete group of a group member" $ do
+      findGroup cs "0" `shouldBe` ["2","0","3","4","6","5"]
+
+  describe "part 1" $ do
+    file <- runIO $ readFile "test/day12.txt"
+
+    it "solves the puzzle" $ do
+      let cs = concat $ map readConnections $ lines file
+      part1 cs "0" `shouldBe` 239
+
+  describe "find groups" $ do
+    let cs = [ ("0", "2"), ("1", "1"), ("2", "0"), ("2", "3"), ("2", "4")
+             , ("3", "2"), ("3", "4"), ("4", "2"), ("4", "3"), ("4", "6")
+             , ("5", "6"), ("6", "4"), ("6", "5") ]
+
+    it "finds all groups" $ do
+      findGroups cs `shouldBe` [["1"], ["2","0","3","4","6","5"]]
+
+  describe "part 2" $ do
+    file <- runIO $ readFile "test/day12.txt"
+
+    it "solves the puzzle" $ do
+      let cs = concat $ map readConnections $ lines file
+      part2 cs `shouldBe` 215
